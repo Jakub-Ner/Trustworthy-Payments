1dd5ec8d726590333324a1c72f3da785220dee46
jakub-ner <kubaner1@gmail.com>
2025-05-31 20:52:11 +0200

    styling and polishing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,15 +8,16 @@ export default function Home() {
   const { account, connect } = useMetaMask();
   let view = <Hello account={account} connect={connect} />;
 
-  const onConfirm = (tokenId: string) => {
-    console.log("Confirming payment with token ID:", tokenId);
+  const onConfirm = (apiKey: string, txnId: string) => {
+    console.log("Confirming payment with API Key:", apiKey);
+    console.log("Confirming payment with token ID:", txnId);
   };
 
   if (!account) view = <PreparePaymentConfirmation onConfirm={onConfirm} />;
 
   return (
     <>
-      <div className="w-full text-center mt-4">
+      <div className="w-full text-center mt-4 absolute">
         <h1 className="text-2xl mt-4 font-bold">Trustworthy Payments</h1>
       </div>
       <div className="flex flex-col items-center justify-center min-h-screen p-4">
--- frontend/src/app/views/preparePaymentConfirmation.tsx
+++ frontend/src/app/views/preparePaymentConfirmation.tsx
@@ -5,11 +5,35 @@ import { useState } from "react";
 export const PreparePaymentConfirmation = ({
   onConfirm,
 }: {
-  onConfirm: (txnId: string) => void;
+  onConfirm: (apiKey: string, txnId: string) => void;
 }) => {
   const [txnId, setTxnId] = useState("");
+  const [apiKey, setApiKey] = useState("")
   return (
     <>
+      <p className="text-center mb-12 max-w-md text-sm text-muted-foreground">
+        🔐 <strong>To confirm your payment</strong>, please provide your Wise API Key and the transaction ID you’d like to verify.
+        <br /><br />
+        🧭 You can find your API Key by following these steps:
+        <br /><br />
+        <span className="inline-block text-left space-y-2">
+          <span className="block pl-6 -indent-6">
+            1. Click your profile picture in the top-right corner of the Wise dashboard.
+          </span>
+          <span className="block pl-6 -indent-6">
+            2. Go to <em>Integrations and tools → API tokens</em>.
+          </span>
+        </span>
+      </p>
+
+      <Textarea
+        placeholder="Enter Your Wise API Key"
+        className="w-full max-w-md mb-4"
+        value={apiKey}
+        onChange={(e) => setApiKey(e.target.value)}
+        readOnly
+      />
+
       <Textarea
         className="w-full max-w-md mb-4"
         placeholder="Enter ID of a Wise transaction you want to generate a payment confirmation for"
@@ -20,10 +44,10 @@ export const PreparePaymentConfirmation = ({
       <Button
         variant="outline"
         className="bg-primary text-white px-4 py-2 rounded-md hover:bg-primary/90 transition"
-        onClick={() => onConfirm(txnId)}
+        onClick={() => onConfirm(apiKey, txnId)}
       >
         Confirm Payment
       </Button>
-      </>
+    </>
   );
 }
